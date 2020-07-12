c9004fa5a0fab5d7126c3e2aa29909980e0af66e
Devlin <webmasterdevlin@gmail.com>
2020-07-12 11:13:56 +0200

    added SyntaxTree

--- compilerX/Program.cs
+++ compilerX/Program.cs
@@ -13,23 +13,33 @@ namespace compilerX
                 Console.Write("> ");
                 var line = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(line))
-                {
                     return;
-                }
-                
+
                 var parser = new Parser(line);
-                var expression = parser.Parse();
+                var syntaxTree = parser.Parse();
 
                 var color = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                PrettyPrint(expression);
+                PrettyPrint(syntaxTree.Root);
                 Console.ForegroundColor = color;
+
+                if (syntaxTree.Diagnostics.Any())
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    foreach (var diagnostic in parser.Diagnostics)
+                    {
+                        Console.WriteLine(diagnostic);
+                    }
+                    Console.ForegroundColor = color;
+                }
             }
         }
 
-        static void PrettyPrint(SyntaxNode node, string indent = "")
+        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
         {
+            var marker = isLast ? "└──" : "├──";
             Console.Write(indent);
+            Console.Write(marker);
             Console.Write(node.Kind);
 
             if (node is SyntaxToken t && t.Value != null)
@@ -40,10 +50,13 @@ namespace compilerX
 
             Console.WriteLine();
 
-            indent += "    ";
+            
+            indent += isLast ? "    " : "│    ";
+            var lastChild = node.GetChildren().LastOrDefault();
+
             foreach (var child in node.GetChildren())
             {
-                PrettyPrint(child, indent);
+                PrettyPrint(child, indent, child == lastChild);
             }
         }
     }
@@ -81,7 +94,7 @@ namespace compilerX
         }
 
         public int Position { get; }
-        public string Text { get; }
+        private string Text { get; }
         public object Value { get; }
     }
 
@@ -89,29 +102,19 @@ namespace compilerX
     {
         private readonly string _text;
         private int _position;
+        private readonly List<string> _diagnostics = new List<string>();
+        public IEnumerable<string> Diagnostics => _diagnostics;
         private char Current => _position >= _text.Length ? '\0' : _text[_position];
 
-        private void Next()
-        {
-            _position++;
-        }
+        private void Next() => _position++;
 
-        public Lexer(string text)
-        {
-            _text = text;
-        }
+        public Lexer(string text) => _text = text;
 
         public SyntaxToken NextToken()
         {
-            // <numbers>
-            // _ - * / ()
-            // <whitespace>
-
             if (_position >= _text.Length)
-            {
                 return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
-            }
-
+            
             if (char.IsDigit(Current))
             {
                 var start = _position;
@@ -128,16 +131,26 @@ namespace compilerX
             }
 
             if (!char.IsWhiteSpace(Current))
-                return Current switch
+                switch (Current)
                 {
-                    '+' => new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null),
-                    '-' => new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null),
-                    '*' => new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null),
-                    '/' => new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null),
-                    '(' => new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null),
-                    ')' => new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null),
-                    _ => new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null)
-                };
+                    case '+':
+                        return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+                    case '-':
+                        return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+                    case '*':
+                        return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+                    case '/':
+                        return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+                    case '(':
+                        return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+                    case ')':
+                        return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                    default:
+                        _diagnostics.Add($"ERROR: bad character input: '{Current}'");
+                        return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1),
+                            null);
+                }
+
             {
                 var start = _position;
 
@@ -148,9 +161,9 @@ namespace compilerX
 
                 var length = _position - start;
                 var text = _text.Substring(start, length);
+                
                 return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
             }
-
         }
     }
 
@@ -163,17 +176,13 @@ namespace compilerX
 
      abstract class ExpressionSyntax : SyntaxNode
     {
-        
     }
 
     sealed class NumberExpressionSyntax : ExpressionSyntax
     {
         public SyntaxToken NumberToken { get; }
 
-        public NumberExpressionSyntax(SyntaxToken numberToken)
-        {
-            NumberToken = numberToken;
-        }
+        public NumberExpressionSyntax(SyntaxToken numberToken) => NumberToken = numberToken;
 
         public override SyntaxKind Kind => SyntaxKind.NumberExpression;
 
@@ -189,10 +198,7 @@ namespace compilerX
         public SyntaxToken OperatorToken { get; }
         public ExpressionSyntax Right { get; }
 
-        public BinaryExpressionSyntax(
-            ExpressionSyntax left,
-            SyntaxToken operatorToken,
-            ExpressionSyntax right)
+        public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right)
         {
             Left = left;
             OperatorToken = operatorToken;
@@ -208,10 +214,25 @@ namespace compilerX
         }
     }
 
+    sealed class SyntaxTree
+    {
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+        }
+    }
+
     class Parser
     {
         private readonly SyntaxToken[] _tokens;
         private int _position;
+        private readonly List<string> _diagnostics = new List<string>();
         public Parser(string text)
         {
             var tokens = new List<SyntaxToken>();
@@ -222,17 +243,20 @@ namespace compilerX
             {
                 token = lexer.NextToken();
                 if (token.Kind != SyntaxKind.WhitespaceToken && token.Kind != SyntaxKind.BadToken)
-                {
-                     tokens.Add(token);
-                }
+                    tokens.Add(token);
+                
             } while (token.Kind != SyntaxKind.EndOfFileToken);
 
             _tokens = tokens.ToArray();
+            _diagnostics.AddRange(lexer.Diagnostics);
         }
 
+        public IEnumerable<string> Diagnostics => _diagnostics;
+        
         private SyntaxToken Peek(int offset)
         {
             var index = _position + offset;
+ 
             return index >= _tokens.Length ? _tokens[^1] : _tokens[index];
         }
 
@@ -247,9 +271,22 @@ namespace compilerX
         
         private SyntaxToken Match(SyntaxKind kind)
         {
-            return Current.Kind == kind ? NextToken() : new SyntaxToken(kind, Current.Position, null, null);
+            if (Current.Kind == kind)
+                return NextToken();
+
+            _diagnostics.Add($"ERROR: Unexpected token <{Current.Kind}>, expected <{kind}>");
+            return new SyntaxToken(kind, Current.Position, null, null);
         }
-        public ExpressionSyntax Parse()
+        
+        public SyntaxTree Parse()
+        {
+            var expression = ParseExpression();
+            var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
+            
+            return new SyntaxTree(_diagnostics, expression, endOfFileToken);
+        }
+
+        private ExpressionSyntax ParseExpression()
         {
             var left = ParsePrimaryExpression();
 
